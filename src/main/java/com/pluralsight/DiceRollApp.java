7ca1dc1a43cd988f44cd5a1ba98045f74a88ec6e
ramenmac <kevinnguyen4200@gmail.com>
2026-04-15 13:11:26 -0500

    Include counter for certain numbers

--- src/main/java/com/pluralsight/DiceRollApp.java
+++ src/main/java/com/pluralsight/DiceRollApp.java
@@ -4,17 +4,35 @@ public class DiceRollApp {
     public static void main(String[] args) {
 
         Dice dice = new Dice();
-        Thread thread = new Thread();
         int roll1, roll2, sum;
+        int twoCounter = 0;
+        int fourCounter = 0;
+        int sixCounter = 0;
+        int sevenCounter = 0;
 
         for (int count = 0; count < 100 ; count++) {
             roll1 = dice.roll();
             roll2 = dice.roll();
             sum = roll1 + roll2;
 
+            switch(sum){
+                case 2:
+                    twoCounter++;
+                    break;
+                case 4:
+                    fourCounter++;
+                    break;
+                case 6:
+                    sixCounter++;
+                    break;
+                case 7:
+                    sevenCounter++;
+                    break;
+                default: continue;
+            }
             outputRoll(roll1, roll2, sum, count);
         }
-
+        printCounter(twoCounter, fourCounter, sixCounter, sevenCounter);
     }
     static void println(String message){
         System.out.println(message);
@@ -23,5 +41,10 @@ public class DiceRollApp {
     static void outputRoll(int roll1 , int roll2 , int sum, int count){
         println("Roll " + count +" :\t" + roll1 + "\t-\t" + roll2 + "\tSum: " + sum);
     }
-    static
+    static void printCounter(int twoCount, int fourCount, int sixCount, int sevenCount){
+        println("Number of 2('s): " + twoCount);
+        println("Number of 4('s): " + fourCount);
+        println("Number of 6('s): " + sixCount);
+        println("Number of 7('s): " + sevenCount);
+    }
 }
